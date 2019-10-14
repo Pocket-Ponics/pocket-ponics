714dbf580b0dd83cdf5ddd19d70b0f7ad727d852
Rohan Patel <pr4h6n@Rohans-MacBook-Pro.local>
2019-10-14 17:16:30 -0400

    Added more comments to lay out the work needed for finishing endpoints

--- packages/pocket-ponics-backend/controllers/mobileappController.js
+++ packages/pocket-ponics-backend/controllers/mobileappController.js
@@ -1,8 +1,13 @@
 //Retrieves all greenhouses for a specific user
 exports.getGreenhouses = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
+
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
+
+    //Retrieve all greenhouse_id values from DB for user_id
+    //TODO: retrieve greenhouse_id values from DB
 
     res.json({
         greenhouses: ["298379827453","872932938740","234234235467"]
@@ -11,15 +16,21 @@ exports.getGreenhouses = (req, res) => {
 
 //Update a specified tier of the greenhouse with provided values
 exports.updateTier = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Store greenhouse_id and tier_id provided
+    var greenhouse_id = req.params.greenhouse_id
+    var tier_id = req.params.tier
+    
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
+    
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
 
-    //new values
-    console.log(req.body)
+    //Insert new values into tier table for given greenhouse_id and tier_id
+    //TODO: insert into DB
 
     res.json({
         200: "OK"
@@ -28,129 +39,201 @@ exports.updateTier = (req, res) => {
 
 //Get all data for specified tier of the greenhouse
 exports.getTier = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
+    
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Store greenhouse_id and tier_id provided
+    var greenhouse_id = req.params.greenhouse_id
+    var tier_id = req.params.tier
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
+
+    //Retrieve values from tier table for given greenhouse_id and tier_id
+    //TODO: query DB
 
     res.json( {plant_id: "029468109359", growth_stage: "2", plant_type_id: "3", water_level: 23.0, light_level: 8.0, cycle_time: 24.0, pH_level: 7.8, elec_cond: 2.1})
 };
 
 //Create a new greenhouse with provided values
 exports.createGreenhouse = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
 
-    //provided values
-    console.log(req.body)
+    //Store greenhouse name provided
+    var greenhouse_name = req.body.name
+
+    // Insert new values into tier table for given greenhouse_name
+    //TODO: insert into DB
 
     res.json({200: "OK"})
 };
 
 //Get values for a greenhouse with specified greenhouse_id
 exports.getGreenhouse = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
+
+    //Store greenhouse_id provided
+    var greenhouse_id = req.params.greenhouse_id
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
 
     res.json({greenhouse_id: "937502957290", user_id: "834058102935", backup_batt_level: 94.0, power_source: 0, tier1: "23", tier2: "34", tier3: "35", tier4: "62", greenhouse_name: "Test Greenhouse"})
 };
 
 //Get readings for a greenhouse with specified greenhouse_id for specified date range
 exports.getGreenhouseReadings = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
+
+    //Store greenhouse_id and date range provided
+    var greenhouse_id = req.params.greenhouse_id
+    var start_date = req.params.start
+    var end_date = req.params.end
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
+
+    //Retrieve sensor readings for greenhouse_id and date range provided
+    //TODO: query DB
 
     res.json({greenhouse_id: "937502957290", user_id: "834058102935", backup_batt_level: [{date: "2016-09-26", reading: 94.0},{date: "2016-09-27", reading: 93.1}], power_source: [{date: "2016-09-26", reading: 0},{date: "2016-09-27", reading: 1}], greenhouse_name: "Test Greenhouse"})
 };
 
 //Update greenhouse with provided values
 exports.updateGreenhouse = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
 
-    //provided values
-    console.log(req.body)
+    //Store greenhouse_id provided
+    var greenhouse_id = req.params.greenhouse_id
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
+
+    //Insert new values into greenhouse table for given greenhouse_id
+    //TODO: insert into DB
 
     res.json({200: "OK"})
 };
 
 //Delete greenhouse with specified greenhouse_id
 exports.deleteGreenhouse = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
+
+    //Store greenhouse_id provided
+    var greenhouse_id = req.params.greenhouse_id
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
+
+    //Delete greenhouse given greenhouse_id
+    //TODO: query DB
 
     res.json({200: "OK"})
 };
 
 //Adjust the water level, nutrient level or light level for specific tier in greenhouse
 exports.makeAdjustments = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
 
-    //provided values
-    console.log(req.body)
+    //Store greenhouse_id and tier_id provided
+    var greenhouse_id = req.params.greenhouse_id
+    var tier_id = req.params.tier
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
+
+    //Store adjustment in DB
+    //TODO: query DB
 
     res.json({200: "OK"})
 };
 
 //Get the current sensor reading for a single sensor type
 exports.getReadingsSingle = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
 
-    res.json({reading: 23.0})
+    //Store greenhouse_id, tier_id and sensor type provided
+    var greenhouse_id = req.params.greenhouse_id
+    var tier_id = req.params.tier
+    var sensor_type = req.params.sensor_type
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
+
+    //Retrieve sensor reading for specified greenhouse_id, tier_id and sensor_type
+    //TODO: query DB
+
+    res.json({reading: 23.4})
 };
 
 //Get all sensor readings for specified tier in greenhouse
 exports.getReadingsTier = (req, res) => {
-    //get auth token
+    //Get auth token
     let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Retrieve user_id for given auth token
+    //TODO: retrieve user_id from DB
+
+    //Store greenhouse_id and tier_id provided
+    var greenhouse_id = req.params.greenhouse_id
+    var tier_id = req.params.tier
+
+    //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+    //TODO: query DB
+
+    //Retrieve sensor readings for specified greenhouse_id and tier_id
+    //TODO: query DB
 
     res.json({water_level: 23.0, pH_level: 7.9, elec_cond: 1.9})
 };
 
 //Get all sensor readings for all tiers of greenhouse
 exports.getReadingsGreenhouse = (req, res) => {
-    //get auth token
-    let cred = req.headers.authorization.split(" ")[1]
-    console.log(cred)
+     //Get auth token
+     let cred = req.headers.authorization.split(" ")[1]
 
-    //get parameters passed through url
-    console.log(req.params)
+     //Retrieve user_id for given auth token
+     //TODO: retrieve user_id from DB
+ 
+     //Store greenhouse_id provided
+     var greenhouse_id = req.params.greenhouse_id
+ 
+     //Check if greenhouse_id is valid for user_id and greenhouse_id provided - do this in the model to make sure nothing gets missed
+     //TODO: query DB
+ 
+     //Retrieve sensor readings for specified greenhouse_id
+     //TODO: query DB
 
     res.json({tier1: {water_level: 23.0, pH_level: 7.9, elec_cond: 1.9}, tier2: {water_level: 23.0, pH_level: 7.9, elec_cond: 1.9}, tier3: {water_level: 23.0, pH_level: 7.9, elec_cond: 1.9}, tier4: {water_level: 23.0, pH_level: 7.9, elec_cond: 1.9},})
 };
--- packages/pocket-ponics-backend/controllers/sensorgridController.js
+++ packages/pocket-ponics-backend/controllers/sensorgridController.js
@@ -2,13 +2,28 @@
 //Post all sensor readings for all tiers of greenhouse
 exports.postReadingsGreenhouse = (req, res) => {
     //convert base64 credentials to ascii
-    let cred = req.headers.authorization.split(" ")[1]
-    let buff = new Buffer(cred, 'base64');
-    let text = buff.toString('ascii');
-    console.log(text)
+    let basicAuth = req.headers.authorization.split(" ")[1]
+    let buff = new Buffer(basicAuth, 'base64');
+    let credentials = buff.toString('ascii').split(":");
 
-    //sensor readings
-    console.log(req.body)
+    //Store username and password provided
+    var serial_no = credentials[0];
+    var password = credentials[1];
+    
+    //Retrieve password hash from database for given serial_number
+    //TODO: retrieve hash from DB
+
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(password, 10);
+
+    //Compare calculated hash to retrieved hash
+    //TODO: compare calculated hash to retrieved hash
+
+    //Retrieve user_id and greenhouse_id for given serial_number
+    //TODO: query DB
+
+    //Store readings in DB
+    //TODO: insert into DB
 
     res.json( {200: "OK"})
 };
@@ -16,33 +31,57 @@ exports.postReadingsGreenhouse = (req, res) => {
 //Post the all the sensor readings for a specific tier
 exports.postReadingsTier = (req, res) => {
     //convert base64 credentials to ascii
-    let cred = req.headers.authorization.split(" ")[1]
-    let buff = new Buffer(cred, 'base64');
-    let text = buff.toString('ascii');
-    console.log(text)
+    let basicAuth = req.headers.authorization.split(" ")[1]
+    let buff = new Buffer(basicAuth, 'base64');
+    let credentials = buff.toString('ascii').split(":");
 
-    //get parameters passed through url
-    console.log(req.params)
+    //Store username and password provided
+    var serial_no = credentials[0];
+    var password = credentials[1];
+    
+    //Retrieve password hash from database for given serial_number
+    //TODO: retrieve hash from DB
 
-    //sensor readings
-    console.log(req.body)
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(password, 10);
+
+    //Compare calculated hash to retrieved hash
+    //TODO: compare calculated hash to retrieved hash
+
+    //Retrieve user_id and greenhouse_id for given serial_number
+    //TODO: query DB
+
+    //Store readings in DB
+    //TODO: insert into DB
 
     res.json( {200: "OK"})
 };
 
 //Post a reading for a single sensor
 exports.postReadingsSingle = (req, res) => {
-    //convert base64 credentials to ascii
-    let cred = req.headers.authorization.split(" ")[1]
-    let buff = new Buffer(cred, 'base64');
-    let text = buff.toString('ascii');
-    console.log(text)
+   //convert base64 credentials to ascii
+   let basicAuth = req.headers.authorization.split(" ")[1]
+   let buff = new Buffer(basicAuth, 'base64');
+   let credentials = buff.toString('ascii').split(":");
 
-    //get parameters passed through url
-    console.log(req.params)
+   //Store username and password provided
+   var serial_no = credentials[0];
+   var password = credentials[1];
+   
+   //Retrieve password hash from database for given serial_number
+   //TODO: retrieve hash from DB
 
-    //sensor readings
-    console.log(req.body)
+   //Calculate hash for provided password
+   var passwordHash = bcrypt.hashSync(password, 10);
+
+   //Compare calculated hash to retrieved hash
+   //TODO: compare calculated hash to retrieved hash
+
+   //Retrieve user_id and greenhouse_id for given serial_number
+   //TODO: query DB
+
+   //Store readings in DB
+   //TODO: insert into DB
 
     res.json( {200: "OK"})
 };
@@ -50,13 +89,28 @@ exports.postReadingsSingle = (req, res) => {
 //Update the greenhouse's current power source
 exports.updatePowerSource = (req, res) => {
     //convert base64 credentials to ascii
-    let cred = req.headers.authorization.split(" ")[1]
-    let buff = new Buffer(cred, 'base64');
-    let text = buff.toString('ascii');
-    console.log(text)
+    let basicAuth = req.headers.authorization.split(" ")[1]
+    let buff = new Buffer(basicAuth, 'base64');
+    let credentials = buff.toString('ascii').split(":");
 
-    //sensor readings
-    console.log(req.body)
+    //Store username and password provided
+    var serial_no = credentials[0];
+    var password = credentials[1];
+    
+    //Retrieve password hash from database for given serial_number
+    //TODO: retrieve hash from DB
+
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(password, 10);
+
+    //Compare calculated hash to retrieved hash
+    //TODO: compare calculated hash to retrieved hash
+
+    //Retrieve user_id and greenhouse_id for given serial_number
+    //TODO: query DB
+
+    //Store readings in DB
+    //TODO: insert into DB
 
     res.json( {200: "OK"})
 };
@@ -64,13 +118,28 @@ exports.updatePowerSource = (req, res) => {
 //Update the current backup battery level
 exports.updateBackupBatteryLevel = (req, res) => {
     //convert base64 credentials to ascii
-    let cred = req.headers.authorization.split(" ")[1]
-    let buff = new Buffer(cred, 'base64');
-    let text = buff.toString('ascii');
-    console.log(text)
+    let basicAuth = req.headers.authorization.split(" ")[1]
+    let buff = new Buffer(basicAuth, 'base64');
+    let credentials = buff.toString('ascii').split(":");
 
-    //sensor readings
-    console.log(req.body)
+    //Store username and password provided
+    var serial_no = credentials[0];
+    var password = credentials[1];
+    
+    //Retrieve password hash from database for given serial_number
+    //TODO: retrieve hash from DB
+
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(password, 10);
+
+    //Compare calculated hash to retrieved hash
+    //TODO: compare calculated hash to retrieved hash
+
+    //Retrieve user_id and greenhouse_id for given serial_number
+    //TODO: query DB
+
+    //Store readings in DB
+    //TODO: insert into DB
 
     res.json( {200: "OK"})
 };
@@ -78,10 +147,28 @@ exports.updateBackupBatteryLevel = (req, res) => {
 //Get any pending adjustment commands from backend
 exports.getAdjustments = (req, res) => {
     //convert base64 credentials to ascii
-    let cred = req.headers.authorization.split(" ")[1]
-    let buff = new Buffer(cred, 'base64');
-    let text = buff.toString('ascii');
-    console.log(text)
+    let basicAuth = req.headers.authorization.split(" ")[1]
+    let buff = new Buffer(basicAuth, 'base64');
+    let credentials = buff.toString('ascii').split(":");
+
+    //Store username and password provided
+    var serial_no = credentials[0];
+    var password = credentials[1];
+    
+    //Retrieve password hash from database for given serial_number
+    //TODO: retrieve hash from DB
+
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(password, 10);
+
+    //Compare calculated hash to retrieved hash
+    //TODO: compare calculated hash to retrieved hash
+
+    //Retrieve user_id and greenhouse_id for given serial_number
+    //TODO: query DB
+
+    //Retrieve adjustments from DB
+    //TODO: query DB
 
     res.json({adjustments: [{type: 0, amount: 34.0}, {type: 1, amount: 4.0},]})
 };
