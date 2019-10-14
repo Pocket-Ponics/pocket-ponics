159e56fb1aad6c4060140f5adf90836ca120fd6f
Rohan <pr4h6n@gmail.com>
2019-10-14 17:17:13 -0400

    Added more comments to lay out the work needed for finishing endpoints

--- packages/pocket-ponics-backend/controllers/authController.js
+++ packages/pocket-ponics-backend/controllers/authController.js
@@ -1,38 +1,81 @@
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 
 //Get an authentication token for given user credentials
 exports.getToken = (req, res) => {
     //convert base64 credentials to ascii
-    let cred = req.headers.authorization.split(" ")[1]
-    let buff = new Buffer(cred, 'base64');
-    let text = buff.toString('ascii');
+    let basicAuth = req.headers.authorization.split(" ")[1]
+    let buff = new Buffer(basicAuth, 'base64');
+    let credentials = buff.toString('ascii').split(":");
+
+    //Store username and password provided
+    var username = credentials[0];
+    var password = credentials[1];
     
-    //calculate hash for provided password
-    var result = bcrypt.hashSync(text.split(":")[1], 10);
-    
-    res.json({token: "d93nlHS134nHWSOOEW"})
+    //Retrieve password hash from database for given username
+    //TODO: retrieve hash from DB
+
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(password, 10);
+
+    //Compare calculated hash to retrieved hash
+    //TODO: compare calculated hash to retrieved hash
+
+    //Calculate token, store in DB and return as response
+    var token = crypto.randomBytes(32).toString('ascii')
+    //TODO: store token in DB
+    res.json({token: token})
 };
 
 //Send a reset password command to backend
 exports.resetPassword = (req, res) => {
-    //provided values
-    console.log(req.body)
+    var username = req.body.username
+    
+    //NOTE: REVOKE TOKENS FOR USER_ID!
 
-    res.json({200: "OK"})
+    res.json({200: "RESET PASSWORD"})
 };
 
 //Create a user with provided username and password
 exports.createUser = (req, res) => {
-    //provided values
-    console.log(req.body)
+    //Store username, email and password provided
+    var username = req.body.username;
+    var password = req.body.password;
+    var email = req.body.email;
+    
+    //Retrieve username from database
+    //TODO: retrieve username from DB
 
-    res.json({200: "OK"})
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(password, 10);
+
+    //If username doesn't exist, create user in DB with username, password hash and email
+
+    res.json({200: "USER CREATED"})
 };
 
 //Change the user's password
 exports.changePassword = (req, res) => {
-    //provided values
-    console.log(req.body)
+    //Store username, old password and new password provided
+    var username = req.body.username;
+    var oldPassword = req.body.old_password;
+    var newPassword = req.body.new_password;
+    
+    //Retrieve password hash from DB for provided username
+    //TODO: retrieve hash from DB
 
-    res.json({200: "OK"})
+    //Calculate hash for provided password
+    var passwordHash = bcrypt.hashSync(oldPassword, 10);
+
+    //Compare calculated hash to retrieved hash
+    //TODO: compare calculated hash to retrieved hash
+
+    //If retrieved password hash matches calculated hash, generate new hash and store in DB
+    //TODO: compare hashes
+    var newPasswordHash = bcrypt.hashSync(newPassword, 10);
+    //TODO: store new hash in DB
+
+    //NOTE: REVOKE TOKENS FOR USER_ID!
+
+    res.json({200: "PASSWORD CHANGED"})
 };
