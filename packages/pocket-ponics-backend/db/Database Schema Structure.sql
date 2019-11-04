c3be342dc69df00afdd99122736b415b6da8c6f3
Rohan <pr4h6n@gmail.com>
2019-11-04 15:48:44 -0500

    Modified file for formatting issue

--- packages/pocket-ponics-backend/db/Database Schema Structure.sql
+++ packages/pocket-ponics-backend/db/Database Schema Structure.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `pocketponics` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `pocketponics`;
 -- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
 --
 -- Host: localhost    Database: pocketponics
@@ -200,4 +202,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-04 13:55:44
+-- Dump completed on 2019-11-04 15:47:42
