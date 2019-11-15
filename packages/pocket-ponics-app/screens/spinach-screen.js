982609847f78be8e4eea1d9291934cf4226fe714
alexcusell1 <acusell2018@fau.edu>
2019-11-14 20:57:34 -0500

    all tier views done

---
+++ packages/pocket-ponics-app/screens/spinach-screen.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { 
+	StyleSheet, 
+	Text, 
+	View, 
+	ImageBackground,
+} from 'react-native';
+
+
+import bgImage from './Spinach.png'
+
+
+export default class Example extends Component {
+	render() {
+		return (
+			<ImageBackground source={bgImage} style={styles.backgroundContainer}>
+			
+			</ImageBackground>
+
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	backgroundContainer: {
+		flex: 1,
+		width: null,
+		height: null,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+});
---
+++ packages/pocket-ponics-app/screens/tom-screen.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { 
+	StyleSheet, 
+	Text, 
+	View, 
+	ImageBackground,
+} from 'react-native';
+
+//import Expo from 'expo';
+//import { StackNavigator } from 'react-navigation';
+
+import bgImage from './tomtier.png'
+
+/* class tierscreen extends react.component {
+	static navigationOptions = {
+		title: 'tier-screen',
+	};
+	render() {
+		const { navigate } = this.props.navigation;
+		return (
+			<View style={styles.container}>
+				<Text
+					onPress= { ()=> navigate('Greenhouse') }>Navigate to greenhouse 
+				</Text>
+			</View>
+		);
+	}
+}
+
+const NavigationApp = StackNavigator({
+	Tier: { screen: tier-screen},
+	Greenhouse: { screen: greenhouse-screen},
+});
+*/
+
+export default class Example extends Component {
+	render() {
+		return (
+			<ImageBackground source={bgImage} style={styles.backgroundContainer}>
+			
+			</ImageBackground>
+
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	backgroundContainer: {
+		flex: 1,
+		width: null,
+		height: null,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+});
