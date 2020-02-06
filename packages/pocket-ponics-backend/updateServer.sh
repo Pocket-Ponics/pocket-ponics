d88713530af76df23b136fcea56ce9c11990367c
Rohan <pr4h6n@gmail.com>
2020-02-06 18:39:54 -0500

    updated refresh script

--- packages/pocket-ponics-backend/updateServer.sh
+++ packages/pocket-ponics-backend/updateServer.sh
@@ -2,8 +2,8 @@
 PATH="/usr/local/bin:/usr/bin:/bin"
 forever stopall
 cd pocket-ponics/packages/pocket-ponics-backend 
+git reset --hard origin/b/rev
 git checkout b/rev
-git reset --hard
 git pull
 npm install
 forever start -c "npm start" ./
