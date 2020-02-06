089c50f033f0a874da9818ad44a8382b8af01a68
Rohan <pr4h6n@gmail.com>
2020-02-06 18:40:57 -0500

    more updates to refresh script

--- packages/pocket-ponics-backend/updateServer.sh
+++ packages/pocket-ponics-backend/updateServer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PATH="/usr/local/bin:/usr/bin:/bin"
 forever stopall
+killall -9 node
 cd pocket-ponics/packages/pocket-ponics-backend 
 git reset --hard origin/b/rev
 git checkout b/rev
