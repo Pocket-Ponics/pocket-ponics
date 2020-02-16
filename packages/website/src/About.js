67d2d7bb1d3fbfcd1c7598507d05aee282e226d4
alexcusell1 <acusell2018@fau.edu>
2020-02-16 14:08:07 -0500

    making requested changes

--- packages/website/src/About.js
+++ packages/website/src/About.js
@@ -3,11 +3,10 @@ import React from 'react'
 export const About = () => (
   <div>
     <p></p>
+
     <h2>Elli Howard</h2>
     <text style={{fontWeight: 'bold'}}>Senior Developer, Project Lead, and Inventor</text>
     <p>I designed this project to synthesize my two major interests, Biology and Computer Science.  As a Biology major, I have both seen and helped with many of the arduous tasks in the agriculture industry.  I felt that some of these tasks, particularly those dealing with hydroponics, could be automated and computerized so they require minimal human intervention.  To that end, I planned out a rough design for an automated greenhouse in my sophomore year, and have been refining the idea ever since. I hope that the team that I have assembled can help me to bring my idea into reality, and I hope that the Pocket ‘Ponics project will go on to help people who are facing food insecurity.</p>
-  	
-  	<p></p>
 
   	<h2>Rohan Patel</h2>
   	<text style={{fontWeight: 'bold'}}> API Stack Developer</text>
@@ -21,7 +20,7 @@ export const About = () => (
   	<text style={{fontWeight: 'bold'}}> Network Designer</text>
   	<p>When this project was pitched to me the team stressed an importance on a stable and easily maintainable sensor grid array, and during my experiences with electrical engineering so far there hasn’t been experience constructing components and circuits that actually serve a purpose and link to a database structure, so I didn’t want to miss the chance to really test and put my electrical engineering skills to the test. I also have had an interest in biology so a chance to work with plants to tweak how they grow and their environments was too fun of an opportunity to pass up.</p>
   	
-	<h2>Matthew Bonsignore</h2>
+	  <h2>Matthew Bonsignore</h2>
   	<text style={{fontWeight: 'bold'}}> Sensor Grid Designer</text>
   	<p>This project is the perfect balance of all my interests and hobbies. I am a current plant hobbyist with several different species that I am maintaining. I have always been into hydroponics and I have always wanted to get a chance to apply it to my current practices. I am also interested in active data collection, and storage maintenance. I am also motivated for this project because of the possible outcomes of this project on a global scale for helping people in need of homegrown produce. Another aspect of this project that motivates me is the skills that I can gain from this project and be able to apply them to my own personal projects.</p>
   	
