8ec7ec707b41b2d915a15ba5d987e0b3e020a350
alexcusell1 <acusell2018@fau.edu>
2020-03-05 16:15:42 -0500

    building new branch

---
+++ packages/website/src/Admin.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import Spacer from 'react-add-space';
+import { render } from 'react-dom';
+import ApiUtil from './util/api-util'
+//import token from './util/api-util'
+
+export function Admin(props){
+
+
+  const [name, setName] = useState('');
+  const [cycle_time, setCycle_time] = useState("");
+  const [light_time, setLight_time] = useState("");
+  const [steps, setSteps] = useState("");
+  const [hsteps, setHsteps] = useState("");
+  const [ph_level_low, setPh_level_low] = useState("");
+  const [ph_level_high, setPh_level_high] = useState("");
+  const [ec_level_low, setEc_level_low] = useState("");
+  const [ec_level_high, setEc_level_high] = useState("");
+  const [temp_low, setTemp_low] = useState("");
+  const [temp_high, setTemp_high] = useState("");
+  const [plant_url, setPlant_url] = useState("");
+  const [harvest_url, setHarvest_url] = useState("");
+  const [num_plants, setNum_plants] = useState("");
+const token = localStorage.getItem('token')
+
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    return ApiUtil.createPlantIdeal(token, ph_level_low, ec_level_low, temp_low, cycle_time, ph_level_high, ec_level_high, temp_high, name, light_time, steps, plant_url, harvest_url, num_plants)
+  } 
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <label> Plant name: </label>
+        <input type="text" name="name" value={name} onChange={event => setName(event.target.value)}/><Spacer amount={51} />
+
+      <label> Number of plants: </label>
+        <input type="text" num_plants="num_plants" value={num_plants} onChange={event => setNum_plants(event.target.value)}/><br /><br />
+
+      <label> Cycle time (days): </label>
+        <input type="text" cycle_time="cycle_time" value={cycle_time} onChange={event => setCycle_time(event.target.value)}/><Spacer amount={40} />
+
+      <label> Ideal light time (mins/day): </label>
+        <input type="text" light_time="light_time" value={light_time} onChange={event => setLight_time(event.target.value)}/><br /><br />
+      
+      <label> Translocation steps: </label>
+        <input type="text" steps="steps" value={steps} onChange={event => setSteps(event.target.value)}/><Spacer amount={38} />
+
+      <label> Harvesting steps: </label>
+        <input type="text" steps="Hsteps" value={hsteps} onChange={event => setHsteps(event.target.value)}/><br /><br />
+
+      <label> Low ph: </label>
+        <input type="text" ph_level_low="ph_level_low" value={ph_level_low} onChange={event => setPh_level_low(event.target.value)}/><Spacer amount={60} />
+
+      <label> High ph:</label>
+        <input type="text" ph_level_high="ph_level_high" value={ph_level_high} onChange={event => setPh_level_high(event.target.value)}/><br /><br />
+
+      <label> Low ec: </label>
+        <input type="text" ec_level_low="ec_level_low" value={ec_level_low} onChange={event => setEc_level_low(event.target.value)}/><Spacer amount={60} />
+
+      <label> High ec:</label>
+        <input type="text" ec_level_high="ec_level_high" value={ec_level_high} onChange={event => setEc_level_high(event.target.value)}/><br /><br />
+
+      <label> Low temp:</label>
+      <input type="text" temp_low="temp_low" value={temp_low} onChange={event => setTemp_low(event.target.value)} /><Spacer amount={55} />
+
+      <label> High temp: </label>
+        <input type="text" temp_high="temp_high" value={temp_high} onChange={event => setTemp_high(event.target.value)}/><br /><br />
+        
+      <label> Plant url: </label>
+        <input type="text" plant_url="plant_url" value={plant_url} onChange={event => setPlant_url(event.target.value)} /><Spacer amount={58} />
+
+      <label> Harvest url:</label>
+        <input type="text" harvest_url="harvest_url" value={harvest_url} onChange={event => setHarvest_url(event.target.value)} /><br />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+render(<Admin />, document.getElementById('root'));
---
+++ packages/website/src/util/api-util.js
@@ -0,0 +1,134 @@
+import base64 from 'base-64'
+
+const host = '10.171.204.187'
+const port = '8080'
+
+const waitingTime = 15000
+
+const APIUtil = {
+	processTextResults(res) {
+		return Promise.resolve(res.text())
+	},
+	put(endpoint, token, body) {
+		return APIUtil.timeoutFetch(waitingTime, fetch(endpoint, {
+			method: 'PUT',
+			headers: new Headers({
+				'Authorization': 'Bearer ' + token,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}),
+			body: APIUtil.urlEncode(body),
+			redirect: 'follow'
+		}))
+			.then(response => response.text())
+			.then(result => {console.log(result); JSON.parse(result)})
+	},
+	urlEncode(object) {
+		var url = []
+		for (var property in object) {
+			var encodedKey = encodeURIComponent(property)
+			var encodedValue = encodeURIComponent(object[property])
+			url.push(encodedKey + '=' + encodedValue)
+		}
+		return url.join('&')
+	},
+	timeoutFetch(ms, promise) {
+		return new Promise((resolve, reject) => {
+			const timeoutId = setTimeout(() => {
+				reject(new Error('API timeout'))
+			}, ms)
+			promise.then(
+				(res) => {
+					clearTimeout(timeoutId)
+					resolve(res)
+				},
+				(err) => {
+					clearTimeout(timeoutId)
+					reject(err)
+				}
+			)
+		})
+	},
+	getAuthToken(username, password) {
+		return APIUtil.timeoutFetch(10000, fetch(`http://${host}:${port}/auth/get_token_admin`, {
+			method: 'GET',
+			headers: new Headers({
+				'Authorization': 'Basic ' + base64.encode(`${username}:${password}`),
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}),
+			redirect: 'follow'
+		}))
+			.then(response => response.text())
+			.then(result => JSON.parse(result))
+	},
+	createUser(email, password) {
+		const encode = APIUtil.urlEncode({ email, password })
+		return APIUtil.timeoutFetch(10000, fetch(`http://${host}:${port}/auth/create_user`, {
+			method: 'POST',
+			headers: new Headers({
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}),
+			body: encode,
+			redirect: 'follow'
+		}))
+			.then(response => response.text())
+			.then(result => {console.log(result); return JSON.parse(result)})
+	},
+	get(endpoint, token, body) {
+		return APIUtil.timeoutFetch(10000, fetch(endpoint, {
+			method: 'GET',
+			headers: new Headers({
+				'Authorization': 'Bearer ' + token,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}),
+			body: APIUtil.urlEncode(body),
+			redirect: 'follow'
+		}))
+			.then(response => response.text())
+			.then(result => JSON.parse(result))
+	},
+	changePassword(token, email, old_password, new_password) {
+		return APIUtil.post(`http://${host}:${port}/auth/change_password`, token, {
+			email,
+			old_password,
+			new_password
+		})
+	},
+	createPlantIdeal(token, ph_level_low, ec_level_low, temp_low, cycle_time, ph_level_high, ec_level_high, temp_high, name, light_time, steps, plant_url, harvest_url, num_plants){
+		console.log("token:", token)
+		return APIUtil.post(`http://${host}:${port}/adminportal`, token,{
+			ph_level_low,
+		    ec_level_low,
+		    temp_low,
+		    cycle_time, 
+		    ph_level_high, 
+		    ec_level_high, 
+		    temp_high, 
+		    name, 
+		    light_time, 
+		    steps, 
+		    plant_url, 
+		    harvest_url,
+		    num_plants
+		})
+	},
+	post(endpoint, token, body) {
+		console.log("body:", body)
+		return APIUtil.timeoutFetch(waitingTime, fetch(endpoint, {
+			method: 'POST',
+			headers: new Headers({
+				'Authorization': 'Bearer ' + token,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}),
+			body: APIUtil.urlEncode(body),
+			redirect: 'follow'
+		}))
+			.then(response => response.text())
+			.then(result => {console.log(result); JSON.parse(result); window.alert(result); window.location.href="http://localhost:3000/AdminHome"})
+
+	},
+	getPlants() {
+		return APIUtil.get(`http://${host}:${port}/adminportal`, '', {})
+	}
+}
+
+export default APIUtil
