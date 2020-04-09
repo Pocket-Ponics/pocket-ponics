568db2038783ee2e33103a5b9b058355df2c05fe
Elli Howard <qwertynerd97@gmail.com>
2020-04-09 11:36:44 -0400

    Final changes for unit tests

---
+++ packages/common/constants.js
@@ -0,0 +1,33 @@
+export const TOMATO_ID = 1
+export const GREENBEAN_ID = 2
+export const SPINACH_ID = 3
+export const TURNIP_ID = 4
+
+export const TOMATO_VALUES = Object.freeze({
+	minPH: 5.5,
+	maxPH: 6.5,
+	minEC: 2.0,
+	maxEC: 5.0,
+	cycleTime: 84
+})
+export const GREENBEAN_VALUES = Object.freeze({
+	minPH: 6.0,
+	maxPH: 6.5,
+	minEC: 2.0,
+	maxEC: 4.0,
+	cycleTime: 65
+})
+export const SPINACH_VALUES = Object.freeze({
+	minPH: 5.5,
+	maxPH: 6.6,
+	minEC: 1.8,
+	maxEC: 2.3,
+	cycleTime: 60
+})
+export const TURNIP_VALUES = Object.freeze({
+	minPH: 6.0,
+	maxPH: 6.5,
+	minEC: 1.8,
+	maxEC: 2.4,
+	cycleTime: 45
+})
